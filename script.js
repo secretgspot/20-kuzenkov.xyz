1181516311ea1bd16224168f3592c11f8f980b1b
Hooker Juggernaut <secretgspot@gmail.com>
2021-02-10 20:31:07 -0600

    ampm hack to normalize

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-// let time = 1;
+// let time = 24;
 let time = (new Date).getHours();
+// let time = (new Date).getSeconds();
 let daylight = time>=5 && time<=17 ? true : false;
+let am = time >= 0 && time <= 12 ? true : false;
 let myHeight = document.documentElement.clientHeight;
+let sun = document.getElementById("sun");
+let sunDay = document.getElementById("sunDay");
+
 
 document.addEventListener('visibilitychange', () => {
   if (document.hidden) {
@@ -14,22 +19,36 @@ document.addEventListener('visibilitychange', () => {
   }
 });
 
+function normalize(val, max, min) { return (val - min) / (max - min); }
+
 function setit() {
+  let sunHeight = sun.clientHeight;
+  let sunRatio = time / sunHeight;
+  let sunPosition = ((time / 24) * 100);
+  console.log(am, daylight, time, sunRatio, sunHeight, sunPosition);
 
-  let sky = document.getElementById("sun");
-  let skyHeight = sky.clientHeight;
-  let skyRatio = time / skyHeight;
-  // document.getElementById("sunDay").style.opacity = (time/myHeight);
-  // document.getElementById("sky").style.opacity = Math.max((time/myHeight), 0.99);
+  sun.style.background = `radial-gradient(circle at 50% ${sunPosition}%, rgba(242,248,247,1) 0%,rgba(249,249,28,1) 3%,rgba(247,214,46,1) 8%, rgba(248,200,95,1) 12%,rgba(201,165,132,1) 30%,rgba(115,130,133,1) 51%,rgba(46,97,122,1) 85%,rgba(24,75,106,1) 100%)`;
 
-  document.getElementById("sun").style.background = 'radial-gradient(circle at 50% ' + ((time / 24) * 100) + '%, rgba(242,248,247,1) 0%,rgba(249,249,28,1) 3%,rgba(247,214,46,1) 8%, rgba(248,200,95,1) 12%,rgba(201,165,132,1) 30%,rgba(115,130,133,1) 51%,rgba(46,97,122,1) 85%,rgba(24,75,106,1) 100%)';
+  sunDay.style.background = `radial-gradient(circle at 50% ${sunPosition}%, rgba(252,255,251,0.9) 0%,rgba(253,250,219,0.4) 30%,rgba(226,219,197,0.01) 70%, rgba(226,219,197,0.0) 70%,rgba(201,165,132,0) 100%)`;
 
-  document.getElementById("sunDay").style.background = 'radial-gradient(circle at 50% ' + ((time / 24) * 100)  + '%, rgba(252,255,251,0.9) 0%,rgba(253,250,219,0.4) 30%,rgba(226,219,197,0.01) 70%, rgba(226,219,197,0.0) 70%,rgba(201,165,132,0) 100%)';
+
+  if (am) {
+    console.log(normalize(time, 12, 0));
+    document.documentElement.style.setProperty('--opaSun', normalize(time, 0, 12));
+    document.documentElement.style.setProperty('--opaSky', normalize(time, 12, 0));
+  } else {
+    console.log(normalize(time, 12, 24));
+    document.documentElement.style.setProperty('--opaSun', normalize(time, 24, 12));
+    document.documentElement.style.setProperty('--opaSky', normalize(time, 12, 24));
+  }
 
   if (daylight) {
-    document.documentElement.style.setProperty('--opa', (time / 24));
+    // console.log(normalize(time, 12, 1));
+    // console.log(normalize(time, 18, 24));
+  //   document.documentElement.style.setProperty('--opaSun', sunRatio);
   } else {
-    document.documentElement.style.setProperty('--opa', ((time / 24) / 2));
+    // console.log(normalize(time, 12, 0));
+  //   document.documentElement.style.setProperty('--opaSun', sunRatio);
   }
 };
 
